77e56c504ff7cd77382fd8bf3687e6bed613525d
Edson Cunha <edson.cunha@ifood.com.br>
2021-11-15 01:40:26 -0300

    Add remote API call timeout

--- src/main/kotlin/io/edsoncunha/cinemademospringboot/adapters/OmdbProperties.kt
+++ src/main/kotlin/io/edsoncunha/cinemademospringboot/adapters/OmdbProperties.kt
@@ -2,10 +2,13 @@ package io.edsoncunha.cinemademospringboot.adapters
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
+import kotlin.properties.Delegates
 
 @Component
 @ConfigurationProperties("omdb")
 class OmdbProperties {
     lateinit var url: String
+    var connectTimeout: Long = 0
+    var readTimeout: Long = 0
     lateinit var apiKey: String
 }
--- src/main/kotlin/io/edsoncunha/cinemademospringboot/configuration/RestTemplateBootstrapper.kt
+++ src/main/kotlin/io/edsoncunha/cinemademospringboot/configuration/RestTemplateBootstrapper.kt
@@ -1,11 +1,16 @@
 package io.edsoncunha.cinemademospringboot.configuration
 
+import io.edsoncunha.cinemademospringboot.adapters.OmdbProperties
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.time.Duration
 
 @Configuration
 class RestTemplateBootstrapper {
     @Bean
-    fun restTemplate(builder: RestTemplateBuilder) = builder.build()
+    fun restTemplate(builder: RestTemplateBuilder, config: OmdbProperties) = builder
+        .setConnectTimeout(Duration.ofMillis(config.connectTimeout))
+        .setReadTimeout(Duration.ofMillis(config.readTimeout))
+        .build()
 }
